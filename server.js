d25ef7eaf9ea87a927fb0f4175547047c51f6337
Vangelis Giannakosian <vangelis@telesto.gr>
2021-05-01 10:43:22 +0300

    adjusted smart app api to my environment

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
 const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.POSTGRES_HOST,
+    user : process.env.POSTGRES_USER,
+    password : process.env.POSTGRES_PASSWORD,
+    database : process.env.POSTGRES_DB
   }
 });
 
