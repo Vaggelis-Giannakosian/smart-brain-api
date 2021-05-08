c523eaf10ef5275b7ef0301fe28856a4c8407822
Vangelis Giannakosian <vangelis@telesto.gr>
2021-05-08 18:25:27 +0300

    signin handler should respond with jwt token along user id

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,56 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
-  }
-  db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
+const jwt = require('jsonwebtoken')
+
+const handleSignin = (db, bcrypt, req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return Promise.reject('incorrect form submission');
+    }
+
+    return db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => user[0])
+                    .catch(err => Promise.reject('unable to get user'))
+            } else {
+                Promise.reject('wrong credentials')
+            }
+        })
+        .catch(err => Promise.reject('wrong credentials'))
+}
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+    const {authorization} = req.headers;
+
+    return authorization ?
+        getAuthTokenId() :
+        handleSignin(db, bcrypt, req, res)
+            .then(data => {
+                return data.id && data.email ?
+                    createSession(data) :
+                    Promise.reject(data)
+            })
+            .then(session => res.json(session))
+            .catch(err => res.status(400).json(err))
+}
+
+function createSession(user) {
+    const {email, id} = user
+    const token = signToken(email)
+    return {success: 'true', id, token};
+}
+
+function signToken(email) {
+    return jwt.sign({email}, process.env.JWTSECRET, {expiresIn: '2 days'});
+}
+
+const getAuthTokenId = () => {
+    console.log('auth ok')
 }
 
 module.exports = {
-  handleSignin: handleSignin
+    signinAuthentication: signinAuthentication
 }
